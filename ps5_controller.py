be03050cd980a567fe747248b9760ce8ac569c34
Mohammed Ali <133220318+Ki11ay@users.noreply.github.com>
2024-08-12 19:11:38 +0300

    Add files via upload

--- ps5_controller.py
+++ ps5_controller.py
@@ -1,4 +1,5 @@
 import serial
+import time
 from evdev import InputDevice, categorize, ecodes
 
 # Connect to Arduino via serial
@@ -8,22 +9,30 @@ arduino = serial.Serial('/dev/ttyUSB0', 9600)
 ps5_controller = InputDevice('/dev/input/event0')
 
 is_rpi = True
+direction_state = {'left': False, 'right': False, 'up': False}
 
-for event in ps5_controller.read_loop():
-    if event.type == ecodes.EV_KEY:
-        if event.code == ecodes.BTN_SOUTH and event.value == 1:  # X button
-            # Send PWM signal to start thrusters at 20% power
-            arduino.write(b'3')  # Assuming '3' triggers 20% power in Arduino code
+def send_signal(signal):
+    arduino.write(signal)
+    time.sleep(5)
 
-        elif event.code == ecodes.BTN_WEST and event.value == 1:  # Left button
-            arduino.write(b'0')
+while True:
+    for event in ps5_controller.read_loop():
+        if event.type == ecodes.EV_KEY:
+            if event.code == ecodes.BTN_DPAD_LEFT:
+                direction_state['left'] = event.value == 1
+            elif event.code == ecodes.BTN_DPAD_RIGHT:
+                direction_state['right'] = event.value == 1
+            elif event.code == ecodes.BTN_DPAD_UP:
+                direction_state['up'] = event.value == 1
+            elif event.code == ecodes.BTN_SELECT and event.value == 1:  # Options button
+                is_rpi = not is_rpi
+                arduino.write(b'4')  # Assuming '4' toggles isRPI in Arduino code
 
-        elif event.code == ecodes.BTN_EAST and event.value == 1:  # Right button
-            arduino.write(b'1')
+    if direction_state['left']:
+        send_signal(b'0')
+    elif direction_state['right']:
+        send_signal(b'1')
+    elif direction_state['up']:
+        send_signal(b'2')
 
-        elif event.code == ecodes.BTN_NORTH and event.value == 1:  # Forward button
-            arduino.write(b'2')
-
-        elif event.code == ecodes.BTN_SELECT and event.value == 1:  # Options button
-            is_rpi = not is_rpi
-            arduino.write(b'4')  # Assuming '4' toggles isRPI in Arduino code
+    time.sleep(0.1)  # Small delay to avoid overwhelming the CPU
